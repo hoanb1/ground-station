df32b0db32eac7051ec4dfb6af16c44e611916e2
sgoudelis <stratos.goudelis@gmail.com>
2025-04-08 12:06:10 +0300

    Refactor database path to use dedicated 'db' directory
    Updated the database file path across the project to ensure a more organized structure by placing it in a dedicated 'db' folder. Adjusted related configurations, `.gitignore`, and deployment script accordingly to align with the new path.

--- backend/db.py
+++ backend/db.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
 # sql alchemy engine
-DATABASE_URL = f"sqlite+aiosqlite:///./{arguments.db}"
+DATABASE_URL = f"sqlite+aiosqlite:///./db/{arguments.db}"
 engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
