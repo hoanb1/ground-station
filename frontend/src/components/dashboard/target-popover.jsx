b766a19b2d1a6be610185d559bd604531e225be0
sgoudelis <stratos.goudelis@gmail.com>
2025-08-03 22:08:38 +0300

    "Adjust SVG height in target popover to 75% for minor style refinement"

--- frontend/src/components/dashboard/target-popover.jsx
+++ frontend/src/components/dashboard/target-popover.jsx
@@ -150,7 +150,7 @@ const SatelliteInfoPopover = () => {
                             backgroundColor: 'rgba(255, 255, 255, 0.08)'
                         },
                         '& svg': {
-                            height: '80%',
+                            height: '75%',
                         }
                     }}
                 >
